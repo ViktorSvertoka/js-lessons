e9c79c3597f651f99d5bf46d9c062344a8df65e0
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-03-14 22:15:10 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -1,47 +1,85 @@
-class Admin {
-  #name;
-  #password;
+// class Admin {
+//   #name;
+//   #password;
 
-  constructor(name, password) {
-    this.#name = name;
-    this.#password = password;
-    this.reversePassword = this.#password.split('').reverse().join('');
-  }
+//   constructor(name, password) {
+//     this.#name = name;
+//     this.#password = password;
+//     this.reversePassword = this.#password.split('').reverse().join('');
+//   }
 
-  get name() {
-    return this.#name;
-  }
+//   get name() {
+//     return this.#name;
+//   }
 
-  //* Розшифровуємо пароль
-  get password() {
-    return this.reversePassword;
-  }
+//   //* Розшифровуємо пароль
+//   get password() {
+//     return this.reversePassword;
+//   }
 
-  //* Зашифровуємо пароль
-  set password(newPassword) {
-    this.#password = newPassword.split('').reverse().join('');
-  }
+//   //* Зашифровуємо пароль
+//   set password(newPassword) {
+//     this.#password = newPassword.split('').reverse().join('');
+//   }
 
-  checkPassword(newPassword) {
-    if (this.reversePassword === newPassword) {
-      console.log('Check is valid');
-    } else {
-      console.log('You are a stranger');
-    }
-  }
+//   checkPassword(newPassword) {
+//     if (this.reversePassword === newPassword) {
+//       console.log('Check is valid');
+//     } else {
+//       console.log('You are a stranger');
+//     }
+//   }
 
-  changePassword(oldPassword, newPassword) {
-    if (this.reversePassword === oldPassword) {
-      this.#password = newPassword.split('').reverse().join('');
-    } else {
-      console.log('Your old password is wrong!');
-    }
-  }
-}
+//   changePassword(oldPassword, newPassword) {
+//     if (this.reversePassword === oldPassword) {
+//       this.#password = newPassword.split('').reverse().join('');
+//     } else {
+//       console.log('Your old password is wrong!');
+//     }
+//   }
+// }
 
-const mango = new Admin('Mango', 'sdvsjdnvskld');
-console.log(mango);
-console.log(mango.name);
+// const mango = new Admin('Mango', 'sdvsjdnvskld');
+// console.log(mango);
+// console.log(mango.name);
 
-console.log(mango.password);
-console.log(mango.checkPassword('sdvsjdnvskld'));
+// console.log(mango.password);
+// console.log(mango.checkPassword('sdvsjdnvskld'));
+
+//!
+
+// const container = document.querySelector('.body');
+
+// console.log(container);
+
+//??
+
+let scores = [56, 78, 988, -67, -444, 65, 84, -34];
+
+//вывесты выграшни значення бильше 0
+// scores.forEach(score => {
+//     if (score > 0) alert(score)
+// })
+
+//======
+//вид выграшных значень видняты 20%
+// scores.forEach(element, index, arrRef) => {
+// if (element > 0) {
+//     arrRef[index] *= 0.8;
+// }
+// }
+
+//======
+// сформуваты новый масыв у долларах
+// const courseDollar = 38;
+// const scoresInDollarList = scores.map(element => element / courseDollar)
+
+//=====
+//сумму всих елементив
+// const totalScore = scores.reduce((prevSum, element) => prevSum + element);
+
+//=====
+//сумму тильки выграшных значень
+const positiveSum = scores.reduce((prevSum, element) =>
+  element > 0 ? prevSum + element : prevSum,
+);
